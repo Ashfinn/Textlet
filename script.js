55bf8898e76fac5f23d97ff7b6710a64e5ee043a
Ashfin <obidur.shawal@gmail.com>
2024-01-01 15:34:26 +0600

    modified:   index.html 	modified:   script.js 	new file:   style.css 	deleted:    styles.css

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function() {
     document.getElementById('sampleeditor').setAttribute('contenteditable', 'true');
 });
+
 function format(command, value) {
     document.execCommand(command, false, value);
 }
+
 function setUrl() {
     let url = document.getElementById('txtFormatUrl').value;
     let stext = document.getSelection();
-
     document.execCommand('insertHTML', false, `<a href="${url}" target="_blank">${stext}</a>`);
     document.getElementById('txtFormatUrl').value = '';
 }
